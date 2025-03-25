5d59d40618a7ab22fde5905ba037c001a8469d75
sansui233 <sansuilnm@gmail.com>
2025-03-25 18:13:57 +0800

    rm command

--- main.py
+++ main.py
@@ -3,18 +3,11 @@ import os
 import typing
 import uuid
 
-from pkg.command import entities as cmd_entities
-from pkg.command.operator import CommandOperator, operator_class
-from pkg.core.entities import Conversation, Query, Session
-from pkg.platform.types.message import MessageChain, Plain
+from pkg.core.entities import Query, Session
 from pkg.plugin.context import APIHost, BasePlugin, EventContext, handler, register
 from pkg.plugin.events import NormalMessageResponded
-from pkg.provider.entities import Message
 from plugins.HistoryOnReload.database import HistoryDataBase
 from plugins.HistoryOnReload.type import ConversationItem, ConversationSchema
-
-db: HistoryDataBase
-db_lock: asyncio.Lock
 
 
 @register(
@@ -30,9 +23,6 @@ class HistoryOnReload(BasePlugin):
         os.makedirs(os.path.dirname(self.db_path), exist_ok=True)
         self.db = HistoryDataBase()
         self.db_lock = asyncio.Lock()
-        # 暴露给其他命令使用
-        db = self.db
-        db_lock = self.db_lock
 
     async def initialize(self):
         async with self.db_lock:
@@ -109,69 +99,6 @@ class HistoryOnReload(BasePlugin):
                 session_name, str(saved_conversation.uuid)
             )
             # self.ap.logger.info(f"[HistoryOnReload] Upsert result: {res}")
-
-
-@operator_class(name="load_hist", help="从历史数据中加载", usage="!load_hist")
-class HistoryReloadCommand(CommandOperator):
-    def __init__(self, host: APIHost):
-        global db, db_lock
-        self.db = db
-        self.db_lock = db_lock
-
-    async def execute(self, ctx: EventContext):
-        if db is None or db_lock is None:
-            return
-        try:
-            # 如果会话全不存在，则从历史记录中加载会话
-            self.ap.logger.info("[HistoryOnReload] 加载历史记录中的活跃会话...\n")
-
-            async with self.db_lock:
-                items = await self.db.get_in_use_conversations()
-
-                for item in items:
-                    conversation_item = typing.cast(
-                        ConversationSchema, item.conversation
-                    )
-                    conversation_item = conversation_item._to_conversation(
-                        use_model=await self.ap.model_mgr.get_model_by_name(
-                            self.ap.provider_cfg.data["model"]
-                        ),
-                        use_funcs=await self.ap.tool_mgr.get_all_functions(
-                            plugin_enabled=True,
-                        ),
-                    )
-                    (launcher_type, launcher_id) = parse_session_name(
-                        item.session_name  # type: ignore
-                    )
-                    session_concurrency = self.ap.system_cfg.data[
-                        "session-concurrency"
-                    ]["default"]
-                    session = Session(
-                        launcher_type=launcher_type,  # type: ignore
-                        launcher_id=launcher_id,
-                        semaphore=asyncio.Semaphore(session_concurrency),
-                    )
-                    session.conversations = [conversation_item]
-                    session.using_conversation = conversation_item
-                    self.ap.sess_mgr.session_list.append(session)
-
-                # 清理数据库中不活跃的会话
-                await self.db.del_item_unused()
-                self.ap.logger.info(f"[HistoryOnReload] 加载 {len(items)} 个活跃会话\n")
-                yield cmd_entities.CommandReturn(
-                    text=f"[HistoryOnReload] 加载 {len(items)} 个活跃会话\n"
-                )
-
-        except Exception as e:
-            if ctx.event.query:
-                await ctx.send_message(
-                    str(ctx.event.query.launcher_type),
-                    str(ctx.event.query.launcher_id),
-                    MessageChain(
-                        [Plain(f"[HistoryOnReload] 加载 {len(items)} 个活跃会话\n")]
-                    ),
-                )
-            raise
 
 
 def parse_session_name(session_name: str) -> tuple[str, str]:
