fbb3375b3b793cf88f31fb64de9fa299107eddec
huangdi <hd20100104@163.com>
2025-07-27 00:16:47 +0800

    fix bug

--- type.py
+++ type.py
@@ -43,7 +43,11 @@ Base = declarative_base()
 class ConversationItem(pydantic.BaseModel):
     """对话，包含于 Session 中，一个 Session 可以有多个历史 Conversation，但只有一个当前使用的 Conversation"""
 
-    pre_prompt: list[llm_entities.Message]
+    pipline_uuid: str
+
+    bot_uuid: str
+
+    prompt: llm_entities.Prompt
 
     messages: list[llm_entities.Message]
 
@@ -66,11 +70,12 @@ class ConversationItem(pydantic.BaseModel):
             create_time=conversation.create_time,
             update_time=conversation.update_time,
             uuid=conversation.uuid,
+            pipline_uuid=conversation.pipeline_uuid,
+            bot_uuid=conversation.bot_uuid,
         )
 
     def _to_conversation(
         self,
-        use_model: entities.LLMModelInfo,
         use_funcs: list[tools_entities.LLMFunction] | None,
     ) -> core_entities.Conversation:
         return core_entities.Conversation(
@@ -78,9 +83,10 @@ class ConversationItem(pydantic.BaseModel):
             messages=self.messages,
             create_time=self.create_time,
             update_time=self.update_time,
-            use_model=use_model,
             use_funcs=use_funcs,
             uuid=self.uuid,
+            pipeline_uuid=self.pipline_uuid,
+            bot_uuid=self.bot_uuid
         )
 
 
